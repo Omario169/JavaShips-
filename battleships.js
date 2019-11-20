a38ec49537785cd182b20592b224f9c6588b1657
Omario169 <47423327+Omario169@users.noreply.github.com>
2019-11-20 20:47:47 +0000

    added extra Javascript code

--- battleships.js
+++ battleships.js
@@ -1,56 +1,56 @@
-// * by 5 will give us numbers between 0 and 5, not including 5. Math floor function will round these numbers to the closest integers. 
-var randomLoc = Math.floor(Math.random() * 5);
-
-// assign the location variables of the ship.
-
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-
-//The value for the users guess for the ships location will be left undefined.
-
-var guess;
-
-//values of 0 are given to hits and guesses to keep track of the users score. 
-
-var hits = 0;
-var guesses = 0;
-
-//The following variable declares if the ship is sunk or not. If true, the ship has sunken. The value is started as false as the ship will initially be generated.  
-
-var isSunk = false;
-
-//user input 
-
-
-
-while (isSunk == false) {
-	guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-	if (guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number!");
-    } else { 
-        guesses = guesses + 1;
-
-        if (guess == location1 || guess == location2 || guess == location3) {
-            alert("HIT");
-            hits = hits + 1;
-            
-
-            if (hits == 3) {
-
-                isSunk = true;
-
-                alert ("You sank my battleship!");
-            }
-        } else {
-            alert("MISS!");
-        }
-        
+let view = {
+    displayMessage: function(msg) {
+        let messageArea = document.getElementById ("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
     }
 }
 
-var stats = "You took " + guesses + " guesses to sink the battleship, " + " which means your shooting accuracy was " + (3/guesses);
+let model = {
+    boardSize: 7,
+    numShips: 3, 
+    shipLength: 3,
+    shipsSunk: 0,
 
-alert (stats);
-		
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+            { locations: ["24", "34", "44"], hits: ["", "", ""] },
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
 
+    fire: function (guess) {
+        for (let i=0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess); //indexOf returns -1 if guess does not match array. 
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit (guess);
+                view.displayMessage ("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed...");
+        return false;
+    },
+
+    isSunk: function (ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+
+                                                                
+};
